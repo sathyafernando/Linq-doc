d4f6dc432593dcc01afbf6ec65a02e9bbfcfeadd
sathya fernando <sathya@inexisconsulting.com>
2018-03-04 05:35:42 +0530

    initial commit

---
+++ LinqFundamental/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Student> students = Student.GetAllStudents().Where(s => s.Gender == "Gender")
+                                          .Where(student => student.Gender == "Male");
+            Console.WriteLine(students);
+
+            /// <summary>
+            ///  Ling query using Sql like query expesions
+            /// </summary>
+            IEnumerable<Student> students1 = from student in Student.GetAllStudents()
+                                             where student.Gender == "Male"
+                                             select student;
+            Console.WriteLine(students1);
+        }
+    }
+}
---
+++ LinqFundamental/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFundamental
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Gender { get; set; }
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            Student student1 = new Student()
+            {
+                Id = 1,
+                Name = "Mahesh",
+                Gender = "Male"
+            };
+            studentList.Add(student1);
+            Student student2 = new Student()
+            {
+                Id = 2,
+                Name = "Isuru",
+                Gender = "Male"
+            };
+            studentList.Add(student2);
+            Student student3 = new Student()
+            {
+                Id = 3,
+                Name = "Sara",
+                Gender = "Female"
+            };
+            studentList.Add(student3);
+
+            return studentList;
+        }
+        // Linq query using lambda expressions
+
+    }
+}
+        
+    
