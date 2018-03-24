7bc3d4356111ed4ac6ed6d7627e7e4cc026d12fb
SathyaFernando <sathya@inexisconsulting.com>
2018-03-24 16:57:18 +0530

    Last Operator Example

---
+++ LinqFundamental/Last.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFundamental
+{
+    public class Last
+    {
+        static void Main(string[] args)
+        {
+            int[] number1 = { };
+            int result1 = number1.Last();
+            Console.WriteLine("Result", result1); //***Throw InvalidOperationException
+
+            int[] number = { 1, 2, 3, 4, 5, 6 };
+            int result = number.Last();
+            Console.WriteLine("Result", result); //Out Put => 6
+
+            int[] number2 = { 1, 2, 3, 4, 5, 6 };
+            int result2 = number2.Last(x =>x%2==0);
+            Console.WriteLine("Result", result2); //Out Put => 6
+
+            int[] number3 = { 1, 2, 3, 4, 5, 6 };
+            int result3 = number3.Last(x => x % 2 == 100);
+            Console.WriteLine("Result", result3); //Out Put => 0
+        }
+    }
+}
