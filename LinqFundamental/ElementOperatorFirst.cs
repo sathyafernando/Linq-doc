de807567feb95284257b528e8c619d4a98f9d32c
SathyaFernando <sathya@inexisconsulting.com>
2018-03-24 15:37:55 +0530

    ElementOperatorFirst

---
+++ LinqFundamental/ElementOperatorFirst.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFundamental
+{
+    public class ElementOperatorFirst
+    {
+        static void Main(string[] args)
+        {
+            int[] number1 = { };
+            int result1 = number1.First();
+            Console.WriteLine("Result", result1); //***Throw InvalidOperationException
+
+            int[] number = { 1, 2, 3, 4, 5, 6 };
+            int result = number.First();
+            Console.WriteLine("Result", result); //Out Put => 1
+        }
+    }
+}
