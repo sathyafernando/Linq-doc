046166e2dc63080f212dbf21cf1482cbbead24d0
SathyaNirmani <36980312+SathyaNirmani@users.noreply.github.com>
2018-03-25 19:42:50 +0530

    Add files via upload

---
+++ LinqFundamental/ConcatesOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOperatorExample
+{
+    public class ConcatesOperator
+    {
+        static void Main(string[] args)
+        {
+            int[] number1 = { 1, 2, 3 };
+            int[] number2 = { 4, 5, 6 };
+
+            var result = number1.Concat(number2); // output => 1,2,3,4,5,6
+        }
+    }
+   
+}
