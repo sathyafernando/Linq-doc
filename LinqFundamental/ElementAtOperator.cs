dedceb06eebc36afd65c95710b26b702c0bc2f1e
SathyaNirmani <36980312+SathyaNirmani@users.noreply.github.com>
2018-03-25 11:43:11 +0530

    Add files via upload

---
+++ LinqFundamental/ElementAtOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOperatorExample
+{
+    public class ElementAtOperator
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = { 1,2,3,4,5 };
+            int result1 = numbers1.ElementAt(1);
+            Console.WriteLine(result1); //OutPut => 2
+
+            int[] numbers = { };
+            int result = numbers.Single();
+            Console.WriteLine(result); //OutPut => Error
+
+            int[] numbers2 = { 1, 2};
+            int result2 = numbers2.ElementAt(2);
+            Console.WriteLine(result2); //OutPut => IndexOutOfBoundException
+
+        }
+    }
+}
