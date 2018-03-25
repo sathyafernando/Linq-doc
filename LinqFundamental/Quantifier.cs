d108b48e4049dd7daccc6a59181f9ca9e3d87175
SathyaNirmani <36980312+SathyaNirmani@users.noreply.github.com>
2018-03-25 22:19:49 +0530

    Add files via upload

---
+++ LinqFundamental/Quantifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOperatorExample
+{
+    public class Quantifier
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            bool result = numbers.Any(); // output=> returns the true as the sequence contains the one 
+
+            bool result1 = numbers.Any(x=>x>5); // returs the false that sequence not contain any element that satisfies given condition
+
+        }
+    }
+}
