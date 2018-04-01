41c2cf9aba90856ee8c62b55e2f88d6d68a9cdaf
SathyaNirmani <36980312+SathyaNirmani@users.noreply.github.com>
2018-04-01 09:42:20 +0530

    Add files via upload

---
+++ LinqFundamental/LinqToXml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinqOperatorExample
+{
+    public class LinqToXml
+    {
+        static void Main(string[] args)
+        {
+            XDocument xmlDocument = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("sdasdas"),
+                new XElement("Student",
+                new XElement("Student", new XAttribute("Id", 101),
+                          new XElement("Name", "Sathya"),
+                          new XElement("Age", 10),
+                          new XElement("Gender", "Female")),
+
+                     new XElement("Student", new XAttribute("Id", 102),
+                          new XElement("Name", "Sara"),
+                          new XElement("Age", 50),
+                           new XElement("Gender", "Female")),
+
+                    new XElement("Student", new XAttribute("Id", 103),
+                          new XElement("Name", "Mahesh"),
+                          new XElement("Age", 90) ,
+                          new XElement("Gender", "Male"))
+                          
+                          ));
+            xmlDocument.Save("C:\\ducument");
+
+
+        }
+    }
+}
