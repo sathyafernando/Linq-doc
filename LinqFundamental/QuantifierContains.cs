1cf06b25c2a078845c3e1b9e9eaaeef7fd8e80bb
SathyaNirmani <36980312+SathyaNirmani@users.noreply.github.com>
2018-04-01 08:14:48 +0530

    Add files via upload

---
+++ LinqFundamental/QuantifierContains.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOperatorExample
+{
+    public class QuantifierContains
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            bool result = numbers.Contains(3); // output=> true
+            
+            string[] countries = {"USA" , "iNDIA" , "UK" };
+
+            bool result1 = countries.Contains("india"); // false
+
+            bool result2 = countries.Contains("india" , StringComparer.OrdinalIgnoreCase); // true
+
+        }
+
+    }
+}
